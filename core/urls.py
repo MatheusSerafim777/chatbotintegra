276c25779e03a9d402982ddff48564772598a7df
Evandro Neto <evandro.rsneto@gmail.com>
2026-03-27 12:00:41 -0300

    fix: update docker-compose configuration for chatbot service and remove unused nginx and certbot services

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from core.api import api
 
@@ -12,3 +13,10 @@ urlpatterns = [
     path('api/', api.urls),
     path('curadoria/', include('respostas_canonicas.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
