8afc31edf7111c4f233ec60e4a359b89321f6a0b
Evandro Neto <evandro.rsneto@gmail.com>
2026-04-10 10:23:09 -0300

    feat: register Mensagem model with custom admin interface and enhanced display options

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -5,7 +5,35 @@ from django.utils.safestring import mark_safe
 from chat.models import ChunkDocumeto, Conversa, Documento, Mensagem
 
 admin.site.register(Documento)
-admin.site.register(Mensagem)
+@admin.register(Mensagem)
+class MensagemAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'conversa',
+        'tipo',
+        'resumo_conteudo',
+        'mensagem_pai',
+        'resposta_canonica',
+        'curtido',
+        'criado_em',
+    )
+    ordering = ('-criado_em',)
+    list_filter = ('tipo', 'curtido', 'criado_em')
+    search_fields = ['conteudo', 'conversa__nome', 'resposta_canonica__pergunta']
+    raw_id_fields = ('conversa', 'mensagem_pai', 'resposta_canonica')
+
+    @admin.display(description='Conteúdo')
+    def resumo_conteudo(self, mensagem: Mensagem):
+        if len(mensagem.conteudo) <= 80:
+            return mensagem.conteudo
+        return f'{mensagem.conteudo[:77]}...'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related(
+            'conversa',
+            'mensagem_pai',
+            'resposta_canonica',
+        )
 
 
 @admin.register(ChunkDocumeto)
