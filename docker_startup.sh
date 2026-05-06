c514d8a9e9cea1234fbd558ffefd5f235131d8a4
Evandro Neto <evandro.rsneto@gmail.com>
2026-05-06 13:02:30 -0300

    fix: update Gunicorn binding port and log level in docker startup script

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o pipefail
 set -o nounset
-
-PORT=${PORT:-8000}
 
 echo "Running migrations..."
 python manage.py migrate --noinput
@@ -14,10 +12,10 @@ python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
 gunicorn core.wsgi \
-  --bind 0.0.0.0:$PORT \
+  --bind 0.0.0.0:8000 \
   --workers 2 \
   --threads 8 \
   --timeout 0 \
-  --log-level debug \
+  --log-level info \
   --access-logfile - \
   --error-logfile -
